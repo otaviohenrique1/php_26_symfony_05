445265c980192a7bc61eae610b700a7591438565
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-05-02 13:04:37 -0300

    commit 4

--- controle_series_symfony/src/MessageHandler/DeleteSeriesImageHandler.php
+++ controle_series_symfony/src/MessageHandler/DeleteSeriesImageHandler.php
@@ -4,17 +4,22 @@ namespace App\MessageHandler;
 
 use App\Message\SeriesWasDeleted;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class DeleteSeriesImageHandler
 {
-  public function __construct(private ParameterBagInterface $parameterBag)
-  {
+  public function __construct(
+    private ParameterBagInterface $parameterBag,
+    private Filesystem $filesystem,
+  ) {
   }
+
   public function __invoke(SeriesWasDeleted $message)
   {
     $coverImagePath = $message->series->getCoverImagePath();
-    unlink($this->parameterBag->get('cover_image_directory') . DIRECTORY_SEPARATOR . $coverImagePath);
+    $this->filesystem->remove($this->parameterBag->get('cover_image_directory') . DIRECTORY_SEPARATOR . $coverImagePath);
+    // unlink($this->parameterBag->get('cover_image_directory') . DIRECTORY_SEPARATOR . $coverImagePath);
   }
 }
